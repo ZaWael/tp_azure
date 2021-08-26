bb2a8855b64eb22556235bd67fad0579f4b9f13b
ZaWael <wael.zamoum@campus-igs-toulouse.fr>
2021-08-26 07:35:21 +0000

    Edited main.tf with correct region/name

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
     features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-    name     = "myTFResourceGroup"
-    location = "westus2"
+resource "azurerm_resource_group" "test_wael" {
+    name     = "test_wael"
+    location = "westeurope"
 }
